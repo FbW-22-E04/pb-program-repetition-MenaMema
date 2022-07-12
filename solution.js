974bea5541845248e09eabea6bc84807cc071eaa
MenaMema <amenabarrera@gmail.com>
2022-07-12 18:22:45 +0200

    finished

---
+++ solution.js
@@ -0,0 +1,24 @@
+let i = 0;
+while (i < 15) {
+  console.log(i);
+  i++;
+}
+
+console.log("-----------------------");
+let j = 1;
+let sum = 0;
+
+while (j <= 20) {
+  sum = sum + j;
+  j++;
+  console.log(sum);
+}
+
+console.log("-----------------------");
+
+let p = 0;
+
+do {
+  console.log("the number is", p);
+  p++;
+} while (p < 20);
